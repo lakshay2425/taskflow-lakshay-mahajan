53520543de6b3b7498fa542e2b22be68ccfc28db
Lakshay <lakshay12290@gmail.com>
2026-04-14 01:29:49 +0530

    refactor: remove default values from config variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 const _config = {
     PORT: process.env.PORT,
-    NODE_ENVIRONMENT: process.env.NODE_ENV || 'development',
+    NODE_ENVIRONMENT: process.env.NODE_ENV,
     dbURI: process.env.DB_URI,
-    JWT_SECRET: process.env.JWT_SECRET || "your-default-secret-key",
-    BYPASS_AUTH: process.env.BYPASS_AUTH || "false",
-    TEST_USER_EMAIL: process.env.TEST_USER_EMAIL || "your-test-email",
-    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
-    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || 12
+    JWT_SECRET: process.env.JWT_SECRET,
+    BYPASS_AUTH: process.env.BYPASS_AUTH,
+    TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS
 }
 
 
